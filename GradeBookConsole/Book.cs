fec110f03e98b3b76691f1f9f805104447ac1fc7
Alvaro Garcia <aagarcia24@hotmail.com>
2021-02-14 20:45:54 -0500

    Finishing module 4

---
+++ GradeBookConsole/Book.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBookConsole
+{
+    public class Book
+    {
+        private List<double> grades;
+        private string name;
+
+        public Book(string name)
+        {
+            grades = new List<double>();
+            this.name = name;
+        }
+        public  void AddGrade(double grade)
+        {
+            grades.Add(grade);
+        }
+
+        public void ShowStatistics()
+        {
+            var result = 0.0;
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
+
+            foreach (var grade in grades)
+            {
+                highGrade = Math.Max(grade, highGrade);
+                lowGrade = Math.Min(grade, lowGrade);
+                result += grade;
+            }
+
+            result /= grades.Count;
+            Console.WriteLine($"The lowest grade is: {lowGrade}");
+            Console.WriteLine($"The highest grade is: {highGrade}");
+            Console.WriteLine($"The average grade is: {result:N1}");
+        }
+    }
+}
